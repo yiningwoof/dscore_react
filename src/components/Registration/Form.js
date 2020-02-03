13773907147cec5110dfc758f6c0e8f96961ff48
Yining Wang <yining.wang@utexas.edu>
2020-02-03 10:25:48 -0600

    created registration components

---
+++ src/components/Registration/Form.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+export const SignUpForm = ({ errors, handleErrors, handleSubmit }) => (
+	<div className="form-container sign-up-container">
+		<form id={'sign-up__form'}>
+			<h1>Create Account</h1>
+			<input
+				className="input"
+				data-colname={'firstname'}
+				type="firstname"
+				placeholder="First name"
+			/>
+			<input
+				className="input"
+				data-colname={'lastname'}
+				type="lastname"
+				placeholder="Last name"
+			/>
+			<input
+				className="input"
+				data-colname={'email'}
+				type="text"
+				placeholder="Email"
+			/>
+			<span className={'error__container'}>
+				{/* {errors + " message goes here"} */}
+			</span>
+			<input
+				className="input"
+				data-colname={'password'}
+				type="password"
+				placeholder="Password"
+			/>
+			<span className={'error__container'}>
+				{/* {errors + " message goes here"} */}
+			</span>
+			<button
+				onClick={(e) => {
+					e.preventDefault();
+					handleSubmit('sign-up__form');
+				}}
+			>
+				Sign Up
+			</button>
+		</form>
+	</div>
+);
+
+export const SignInForm = ({ errors, handleErrors, handleSubmit }) => (
+	<div className="form-container sign-in-container">
+		<form id={'sign-in__form'}>
+			<h1>Sign in</h1>
+			<input
+				className="input"
+				data-colname={'email'}
+				type="name"
+				placeholder="Email"
+			/>
+			<input
+				className="input"
+				data-colname={'password'}
+				type="password"
+				placeholder="Password"
+			/>
+			<button
+				onClick={(e) => {
+					e.preventDefault();
+					handleSubmit('sign-in__form');
+				}}
+			>
+				Sign In
+			</button>
+		</form>
+	</div>
+);
---
+++ src/components/Registration/Registration.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import { SignUpForm, SignInForm } from './Form';
+import { SignInOverlay } from './Overlay';
+// import { handleUserFormSubmit } from '../../helpers';
+import './styles.css';
+
+export class Registration extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			errors: ['error state']
+		};
+	}
+
+	handleErrors = (errObj) => {
+		const newErrObj = Object.keys(errObj.message).map((key) => {
+			return {
+				key,
+				message: errObj.message[key][0]
+			};
+		});
+
+		this.setState({
+			...this.state,
+			errors: [...this.state.errors, ...newErrObj]
+		});
+	};
+
+	handleSubmit = async (formId) => {
+		const { setUser } = this.props; // references the setUser hook in App.js
+		const APIUrl = `http://localhost:3000/api/v1/users`;
+
+		if (formId === 'sign-up__form') {
+			await this.handleUserFormSubmit(formId, setUser, APIUrl);
+		} else {
+			await this.handleUserFormSubmit(formId, setUser, `${APIUrl}/login`);
+		}
+	};
+
+	handleUserFormSubmit = async (form, setUserState, APIUrl) => {
+		const formEl = document.querySelector(`#${form}`); //find form that user filled out
+		const inputsArr = formEl.querySelectorAll('input'); // gets a list of all inputs on above form
+		let userObject = {}; // initializing user obj
+
+		inputsArr.forEach((input) => {
+			//loop through inputs
+			userObject[input.dataset.colname] = input.value; // build user obj with input values
+		});
+
+		const fetchOptions = {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ user: userObject })
+		};
+
+		await fetch(APIUrl, fetchOptions) // make fetch post request that waits for api res
+			.then((res) => {
+				//then when res comes back check res status for code
+				if (res.status === 409) {
+					// conflict res'
+
+					res
+						.text()
+						.then((errs) => {
+							throw new Error(errs);
+						})
+						.catch((err) => {
+							const errJSON = JSON.parse(err.message);
+							return errJSON;
+						});
+				} else if (res.status === 403) {
+					// unauthorized res
+
+					res
+						.text()
+						.then((errs) => {
+							throw new Error(errs);
+						})
+						.catch((err) => {
+							const errJSON = JSON.parse(err.message);
+							return errJSON;
+						});
+				} else {
+					res.json().then((user) => {
+						setUserState(user); // references the setUser hook in signIn.js
+					});
+				}
+			});
+	};
+
+	render() {
+		const { errors } = this.state;
+
+		return (
+			<div>
+				<div className="container" id="container">
+					<SignUpForm
+						errors={errors}
+						handleErrors={this.handleErrors}
+						handleSubmit={this.handleSubmit}
+					/>
+					<SignInForm
+						errors={errors}
+						handleErrors={this.handleErrors}
+						handleSubmit={this.handleSubmit}
+					/>
+					<SignInOverlay />
+				</div>
+			</div>
+		);
+	}
+}
