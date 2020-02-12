ce79196bf58887871dc0438bb85a1c18ac65a301
Yining <yining.wang@utexas.com>
2020-02-11 23:36:01 -0600

    home page nav bar and rest styling, registration textfield on error styling

--- src/App2.js
+++ src/App2.js
@@ -1,29 +1,29 @@
-import React, { useEffect } from 'react';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import {
-	signOut,
-	getUser,
-	getHoles,
-	getGame,
-	getAllRounds,
-	getAllScores
-} from './actions';
+  signOut,
+  getUser,
+  getHoles,
+  getGame,
+  getAllRounds,
+  getAllScores
+} from "./actions";
 // import { connect } from "react-redux";
-import axios from 'axios';
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
 
-import { Nav } from './components/Nav/Nav';
-import { Home } from './components/Home/Home';
+import { Nav } from "./components/Nav/Nav";
+import { Home } from "./components/Home/Home";
 // import { CurrentGame } from "./components/CurrentGame/CurrentGame";
 // import Leaderboard from "./components/Leaderboard/Leaderboard";
-import { Registration } from './components/Registration/Registration';
-import { SignInButton } from './components/SignInButton/SignInButton';
-import { NewGame } from './components/NewGame/NewGame';
-import { CurrentGame } from './components/CurrentGame/CurrentGame';
-import { Leaderboard } from './components/Leaderboard/Leaderboard';
+import { Registration } from "./components/Registration/Registration";
+import { SignInButton } from "./components/SignInButton/SignInButton";
+import { NewGame } from "./components/NewGame/NewGame";
+import { CurrentGame } from "./components/CurrentGame/CurrentGame";
+import { Leaderboard } from "./components/Leaderboard/Leaderboard";
 
 // STORE -> GLOBALIZED STATE
 // ACTION
@@ -31,57 +31,57 @@ import { Leaderboard } from './components/Leaderboard/Leaderboard';
 // DISPATCH
 
 function App() {
-	const loggedUser = useSelector((state) => state.getUser);
-	const game = useSelector((state) => state.getGame);
-	//   const playerNames = useSelector(state => state.getPlayerNames);
-	const dispatch = useDispatch();
+  const loggedUser = useSelector(state => state.getUser);
+  const game = useSelector(state => state.getGame);
+  //   const playerNames = useSelector(state => state.getPlayerNames);
+  const dispatch = useDispatch();
 
-	const signOutUser = () => {
-		dispatch({
-			type: 'INIT_GAME'
-		});
-		dispatch({
-			type: 'INIT_ROUNDS'
-		});
-		dispatch({
-			type: 'INIT_SCORES'
-		});
-		dispatch(signOut());
-	};
+  const signOutUser = () => {
+    dispatch({
+      type: "INIT_GAME"
+    });
+    dispatch({
+      type: "INIT_ROUNDS"
+    });
+    dispatch({
+      type: "INIT_SCORES"
+    });
+    dispatch(signOut());
+  };
 
-	useEffect(() => {
-		dispatch(getUser());
-		dispatch(getHoles());
-		dispatch(getAllRounds());
-		// dispatch(getAllScores());
-	}, [game]);
+  useEffect(() => {
+    dispatch(getUser());
+    dispatch(getHoles());
+    dispatch(getAllRounds());
+    // dispatch(getAllScores());
+  }, [game]);
 
-	return (
-		<div>
-			<Nav signOut={signOutUser} />
-			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="/current_game">
-					<CurrentGame />
-				</Route>
-				<Route path="/leaderboard">
-					<Leaderboard />
-				</Route>
-				<Route path="/registration">
-					{loggedUser.user && loggedUser.user.id ? (
-						<Redirect to="/" />
-					) : (
-						<Registration />
-					)}
-				</Route>
-				<Route path="/new_game">
-					<NewGame />
-				</Route>
-			</Switch>
-		</div>
-	);
+  return (
+    <>
+      <Nav signOut={signOutUser} />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/current_game">
+          <CurrentGame />
+        </Route>
+        <Route path="/leaderboard">
+          <Leaderboard />
+        </Route>
+        <Route path="/registration">
+          {loggedUser.user && loggedUser.user.id ? (
+            <Redirect to="/" />
+          ) : (
+            <Registration />
+          )}
+        </Route>
+        <Route path="/new_game">
+          <NewGame />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 // export default connect(null, { getUser })(App);
